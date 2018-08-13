cab8a863b85ae6d95c0742462c644ccfec5a9c03
Jason Young <cm.jasonyoung@gmail.com>
2018-08-13 11:00:50 -0400

    Formatted the react files, fixed some lint errors, and added a POC websocket implementation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,61 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import ReconnectingWebSocket from 'reconnecting-websocket';
 
 import Alert from './components/Alert';
 
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.rws;
+    this.state = {
+      count: null,
+      alerts: []
+    };
+    this.updateData = this.updateData.bind(this);
+  }
 
+  componentDidMount() {
+    this.rws = new ReconnectingWebSocket(`ws://${window.location.hostname}:3001`);
+    this.rws.addEventListener('message', this.updateData);
+  }
 
+  componentWillUnmount() {
+    this.rws.removeEventListener('message', this.updateData);
+  }
 
-class App extends Component {
+  updateData(message) {
+    const { count, data: alerts = [] } = JSON.parse(message.data);
+    this.setState({ count, alerts });
+  }
 
-
-constructor() {
-super()
-
-
-this.state = {
-
-
-alerts: []
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p>
+          Count:
+          &nbsp;
+          {this.state.count}
+        </p>
+        <div className="flex-row">
+          {
+            this.state.alerts.map((alert, index) => {
+              const { type, affectedZones, event, headline, description } = alert
+              const zones = affectedZones.join(',')
+              return (
+                <Alert key={`${index}-${zones}`} type={type} event={event} headline={headline} description={description} zones={zones} />
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
-}
-
-
-componentDidMount() {
-
-
-this.getAlerts()
-}
-
-
-getAlerts() {
-
-
-fetch('/api/active')
-.then(res => res.json())
-.then(alerts => this.setState({ alerts }));
-}
-
-
-render() {
-
-
-return (
-<div className="App">
-<header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<h1 className="App-title">Welcome to React</h1>
-</header>
-<div className="flex-row">
-{
-this.state.alerts.map(alert => {
-const { type, affectedZones, event, headline, description } = alert
-return (
-<Alert type={type} event={event} headline={headline} description={description} zones={affectedZones.join(',')} />
-)
-})
-}
-</div>
-
-
-</div>
-
-
-);
-}
-}
-
 
 export default App;
--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 
-
 class Alert extends Component {
-
-
-
-
-render() {
-
-
-const { type, event, description, zones, headline } = this.props;
-return (
-<div class="flex-item">
-<div class={type}>{type}</div>
-<div><strong>{event}</strong></div>
-<textarea>{zones}</textarea>
-<p>{description}</p>
-</div>
-)
+  render() {
+    const { type, event, description, zones, headline } = this.props;
+    return (
+      <div className="flex-item">
+        <div className={type}>{type}</div>
+        <div><strong>{event}</strong></div>
+        <textarea defaultValue={zones}></textarea>
+        <p>{description}</p>
+      </div>
+    )
+  }
 }
-}
-
 
 export default Alert;
